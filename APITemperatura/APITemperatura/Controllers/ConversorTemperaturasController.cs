9dc3581ebcac2f3c2b00ea4cda043525e2e47a29
GustavoSantOliveira <gustavosantana86@hotmail.com>
2019-05-20 22:59:50 -0300

    push para repositorio

---
+++ APITemperatura/APITemperatura/Controllers/ConversorTemperaturasController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APITemperatura.Controllers
+{
+    [Route("api/[controller]")]
+    public class ConversorTemperaturasController : Controller
+    {
+        /// <summary>
+        /// Transforma uma temperatura em Celsius para o equivalente
+        /// nas escalas Fahrenheit e Kelvin.
+        /// </summary>
+        /// <param name="temperatura">Temperatura em Celsius</param>
+        /// <returns>Objeto contendo valores para uma temperatura
+        /// nas escalas Celsius, Fahrenheit e Kelvin.</returns>
+        [HttpGet("Celsius/{temperatura}")]
+        public object GetConversaoCelsius(double temperatura)
+        {
+            Temperatura dados = new Temperatura();
+            dados.ValorCelsius = temperatura;
+            dados.ValorFahrenheit =
+                Math.Round((temperatura * 9 / 5) + 32);
+            dados.ValorKelvin = dados.ValorCelsius + 273.15;
+
+            return dados;
+        }
+    }
+}
